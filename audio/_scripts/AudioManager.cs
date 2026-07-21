2b67b1e477fca053e8878c3b3ebc2f6d111ff593
jdugan0 <justin.d.dugan@gmail.com>
2026-07-21 11:23:34 -0400

    updated audiomanager

--- audio/_scripts/AudioManager.cs
+++ audio/_scripts/AudioManager.cs
@@ -1,14 +1,18 @@
-using Godot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Godot;
 
 public partial class AudioManager : Node
 {
-    [Export] public Sound[] sounds;
-    private Dictionary<string, Sound> dict = new Dictionary<string, Sound>();
+    [Export]
+    public Sound[] sounds;
+    private Dictionary<string, Sound> dict = new();
     public static AudioManager instance;
-    public List<KeyValuePair<string, AudioStreamPlayer>> playing = new List<KeyValuePair<string, AudioStreamPlayer>>();
+    public Dictionary<Guid, AudioStreamPlayer> playing = new();
+    public Dictionary<string, List<(AudioStreamPlayer p, Guid id)>> playingByName = new();
+    public Dictionary<Guid, string> names = new();
 
     public override void _Ready()
     {
@@ -25,7 +29,8 @@ public partial class AudioManager : Node
             dict.Add(s.name, s);
         }
     }
-    public AudioStreamPlayer PlaySFX(Node from, string sound, float time)
+
+    public (AudioStreamPlayer, Guid) PlaySFX(Node from, string sound, float time)
     {
         var player = new AudioStreamPlayer();
         Sound s;
@@ -33,49 +38,77 @@ public partial class AudioManager : Node
         player.Stream = s.stream;
         player.VolumeDb = s.volume;
         // playing.Add(s, player);
-        player.Finished += () => playing.Remove(new KeyValuePair<string, AudioStreamPlayer>(s.name, player));
+        Guid id = Guid.NewGuid();
+        player.Finished += () => playing.Remove(id);
+        player.Finished += () => playingByName.Remove(sound);
+        player.Finished += () => names.Remove(id);
         player.Finished += () => player.QueueFree();
-        playing.Add(new KeyValuePair<string, AudioStreamPlayer>(s.name, player));
         // player.Finished += ()=>playing.Remove(s);
         from.AddChild(player);
+        playing.Add(id, player);
+        if (!playingByName.ContainsKey(sound))
+        {
+            playingByName.Add(sound, new());
+        }
+        playingByName[sound].Add((player, id));
+        names.Add(id, sound);
         player.Play(time);
-        return player;
+        return (player, id);
     }
-    public AudioStreamPlayer PlaySFX(Node from, string sound)
+
+    public (AudioStreamPlayer, Guid) PlaySFX(Node from, string sound)
     {
         return PlaySFX(from, sound, 0);
     }
-    public AudioStreamPlayer PlaySFX(string sound)
+
+    public (AudioStreamPlayer, Guid) PlaySFX(string sound)
     {
         return PlaySFX(this, sound, 0);
     }
-    public void CancelSFX(string sound)
+
+    public (AudioStreamPlayer, Guid) PlaySFX(string sound, float time)
     {
-        if (isPlaying(sound))
-        {
-            KeyValuePair<string, AudioStreamPlayer> p = new KeyValuePair<string, AudioStreamPlayer>();
-            foreach (KeyValuePair<string, AudioStreamPlayer> pair in playing)
-            {
-                if (pair.Key == sound)
-                {
-                    p = pair;
-                    break;
-                }
-            }
-            playing.Remove(p);
-            p.Value.QueueFree();
-        }
-    }
-    public bool isPlaying(string sound)
-    {
-        foreach (KeyValuePair<string, AudioStreamPlayer> pair in playing)
-        {
-            if (pair.Key == sound)
-            {
-                return true;
-            }
-        }
-        return false;
+        return PlaySFX(this, sound, time);
     }
 
+    public (bool, AudioStreamPlayer) CancelSFX(Guid id)
+    {
+        if (IsPlaying(id))
+        {
+            var p = playing[id];
+            playing.Remove(id);
+            playingByName.Remove(names[id]);
+            names.Remove(id);
+            p.QueueFree();
+            return (true, p);
+        }
+        return (false, null);
+    }
+
+    public (bool, AudioStreamPlayer) CancelSFX(string sound)
+    {
+        foreach (var s in GetPlaying(sound))
+        {
+            playingByName.Remove(sound);
+            playing.Remove(s.id);
+            names.Remove(s.id);
+            s.p.QueueFree();
+            return (true, s.p);
+        }
+        return (false, null);
+    }
+
+    public List<(AudioStreamPlayer p, Guid id)> GetPlaying(string sound)
+    {
+        if (playingByName.ContainsKey(sound))
+        {
+            return playingByName[sound];
+        }
+        return new List<(AudioStreamPlayer, Guid)>();
+    }
+
+    public bool IsPlaying(Guid id)
+    {
+        return playing.ContainsKey(id);
+    }
 }
