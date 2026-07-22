49fe51530722ac048c27d5ab557414aa61a5bc45
jdugan0 <justin.d.dugan@gmail.com>
2026-07-22 15:47:19 -0400

    camera

--- scripts/Movement.cs
+++ scripts/Movement.cs
@@ -13,12 +13,22 @@ public partial class Movement : CharacterBody2D
 
     public bool moveEnabled = true;
 
+    [Export]
+    private Camera2D camera;
+
+    [Export]
+    private float mouseCameraWeight;
+
     public override void _PhysicsProcess(double delta)
     {
         float dt = (float)delta;
 
         Vector2 input = Input.GetVector("LEFT", "RIGHT", "UP", "DOWN");
         Vector2 targetVelocity = input * MaxSpeed;
+
+        camera.GlobalPosition =
+            (GlobalPosition + mouseCameraWeight * GetGlobalMousePosition())
+            / (1 + mouseCameraWeight);
 
         float rate = input == Vector2.Zero ? Friction : Acceleration;
         Velocity = Velocity.MoveToward(targetVelocity, rate * dt);
