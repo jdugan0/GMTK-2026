8d8fca83559dcf549a31a629daf598cf81734deb
jdugan0 <justin.d.dugan@gmail.com>
2026-07-26 10:30:45 -0400

    crae anim

--- scripts/Crate.cs
+++ scripts/Crate.cs
@@ -1,4 +1,20 @@
 using System;
 using Godot;
 
-public partial class Crate : Node { }
+public partial class Crate : Node2D
+{
+	[Export]
+    Texture2D destroy;
+
+    [Export]
+    AnimatedSprite2D cpy;
+
+    public void Destroy()
+    {
+        var n = new Sprite2D();
+        n.Texture = destroy;
+        n.Scale = cpy.Scale;
+        GetTree().CurrentScene.AddChild(n);
+        n.GlobalPosition = cpy.GlobalPosition;
+    }
+}
